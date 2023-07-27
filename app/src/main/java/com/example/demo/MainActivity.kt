fefa4cf2de2d7f4f751f160c0024f8cef36b9bb6
AryanP107 <aryanpanchal107@gmail.com>
2023-07-27 23:30:01 +0530

    First UI

---
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        var x = findViewById<Button>(R.id.button)
+        x.setOnClickListener {
+            Toast.makeText(this@MainActivity,"hello bro",Toast.LENGTH_LONG).show()
+        }
+    }
+}
